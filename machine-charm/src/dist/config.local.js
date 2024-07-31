0c96eb754a5187f290fe1974a8e7a69a59464cbd
vladimir-cucu <vladimir.cucu@canonical.com>
2024-07-31 16:07:25 +0300

    temp: update dashboard to 0.13.0

---
+++ machine-charm/src/dist/config.local.js
@@ -0,0 +1,38 @@
+// multipass juju-controller
+var jujuDashboardConfig = {
+  controllerAPIEndpoint: "wss://10.160.245.219:17070/api",
+  baseAppURL: "/",
+  identityProviderURL: "",
+  isJuju: true,
+  analyticsEnabled: false,
+};
+
+// multipass juju-candid
+// var jujuDashboardConfig = {
+//   controllerAPIEndpoint: "wss://10.160.245.118:17070/api",
+//   baseAppURL: "/",
+//   identityProviderURL: "/",
+//   isJuju: true,
+//   analyticsEnabled: false,
+// };
+
+// JIMM staging
+// var jujuDashboardConfig = {
+//   controllerAPIEndpoint:
+//     "wss://jimm.comsys-internal.v2.staging.canonical.com/api",
+//   baseAppURL: "/",
+//   identityProviderAvailable: true,
+//   identityProviderURL: "",
+//   isJuju: false,
+//   analyticsEnabled: false,
+// };
+
+// JIMM demo
+// var jujuDashboardConfig = {
+//   controllerAPIEndpoint: "wss://jimm.comsys-internal.v2.demo.canonical.com/api",
+//   baseAppURL: "/",
+//   identityProviderAvailable: true,
+//   identityProviderURL: "",
+//   isJuju: false,
+//   analyticsEnabled: false,
+// };
